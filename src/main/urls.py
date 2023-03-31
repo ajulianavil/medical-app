12caf25309287bee6fb3d572fb1c7c2f05bb0ba9
Chiasaurio <33266101+Chiasaurio@users.noreply.github.com>
2023-03-31 12:36:22 -0500

    Revert "Add existing project files to Git"
    This reverts commit 6d77b5d242dcd7d05eb13ecb4b0dc3533df3df40.

--- src/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("consultas/nueva", views.agregar_consulta, name="consultas/nueva")
-]
