b6fcd78b9d54936a7f7de29f9f13043cdfc83bb4
ajulianavil <ajulianavil@hotmail.com>
2023-04-09 12:05:40 -0500

    username

--- src/users/models.py
+++ src/users/models.py
@@ -20,7 +20,7 @@ class Appuser(AbstractUser):
     description = models.TextField("Description", max_length=600, default='', blank=True)
     userid = models.AutoField( primary_key=True)  # Field name made lowercase.
     last_login = models.DateTimeField( blank=True, null=True)
-    # password = models.CharField( max_length=100, blank=True, null=True)  # Field name made lowercase.
+    password = models.CharField( max_length=100, blank=True, null=True)  # Field name made lowercase.
     savedate = models.DateTimeField( auto_now_add=True, blank=True, null=True)  # Field name made lowercase.
 
     USERNAME_FIELD = 'email'
