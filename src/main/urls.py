9db33dae387fca2acc93d666a127275fa5b67206
ajulianavil <ajulianavil@hotmail.com>
2023-05-05 08:22:35 -0500

    Fixes ui

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("landing", views.landing, name="landing"),
+    path("", views.landing, name="landing"),
     path("aboutUs", views.aboutUs, name="aboutUs"),
     path("howToRegister", views.howToRegister, name="howToRegister")    
 ]
--- src/ultrasonido_app/static/js/reporte_script.js
+++ src/ultrasonido_app/static/js/reporte_script.js
@@ -1,10 +1,3 @@
-
-// window.onload = function () {
-//     var e = document.getElementById("date");
-//     var dateString = e.getAttribute("data-my-date");
-//     console.log(dateString);
-//     var date = new Date(dateString);
-//     console.log(dateString);
-//     var formattedDate = date.toLocaleDateString("en-US", { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, "-");
-//     e.value = formattedDate;
-// };
+function logSomething() {
+    console.log("Something");
+  }
