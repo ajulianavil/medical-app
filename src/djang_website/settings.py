96ef6fb4c302c1acc54783f6fb762e978a78856d
daniel4nnah <laura.svilla21@gmail.com>
2023-06-04 18:10:46 -0500

    Cambios de correo

--- src/djang_website/settings.py
+++ src/djang_website/settings.py
@@ -193,8 +193,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'andreschiia1@gmail.com'
-EMAIL_HOST_PASSWORD = 'srreznzylofklfyu'
+EMAIL_HOST_USER = 'neuroapp.contacto@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxppjhvxaevdzlcz'
 
 # GLOBAL VARIABLES
 CM_REF = 10
